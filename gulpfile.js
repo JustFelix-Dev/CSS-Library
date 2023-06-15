78062a71f3cd35ee260a75853296e459cf4b37ea
Felix Owolabi <owolabifelix78@gmail.com>
2023-06-15 10:57:04 +0100

    CSS-Library

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+  const { src,watch,dest,series } = require('gulp')
+  const sass = require('gulp-sass')(require('sass'))
+  const purgecss = require('gulp-purgecss')
+
+  function buildStyles(){
+      return src('felix/**/*.scss')
+      .pipe(sass())
+      .pipe(purgecss({content:['*.html']}))
+      .pipe(dest('css'))
+  }
+
+  function watchTask(){
+    watch(['felix/**/*.scss'],buildStyles)
+  }
+
+  exports.default = series(buildStyles,watchTask)
