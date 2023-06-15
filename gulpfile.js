6ea224a6c09a36daddae016d4aef577b536279bc
Felix Owolabi <owolabifelix78@gmail.com>
2023-06-15 11:42:10 +0100

    Build gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,15 @@
   const { src,watch,dest,series } = require('gulp')
   const sass = require('gulp-sass')(require('sass'))
   const purgecss = require('gulp-purgecss')
+  // const deploy = require('gulp-gh-pages');
+
+  // gulp.task('deploy', function () {
+  //   return gulp.src("./prod/**/*")
+  //     .pipe(deploy({ 
+  //       remoteUrl: "https://github.com/JustFelix-Dev/JustFelix-Dev.github.io.git",
+  //       branch: "master"
+  //     }))
+  // });
 
   function buildStyles(){
       return src('felix/**/*.scss')
@@ -14,3 +23,4 @@
   }
 
   exports.default = series(buildStyles,watchTask)
+  exports.build = series(buildStyles,watchTask)
