44071e2b397723131ba3e4d165efc520dea5024d
Felix Owolabi <owolabifelix78@gmail.com>
2023-07-17 13:40:45 +0100

    cssLibrary

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,18 @@
-  const { src,watch,dest,series } = require('gulp')
-  const sass = require('gulp-sass')(require('sass'))
-  const purgecss = require('gulp-purgecss')
+    const { src,watch,dest,series } = require('gulp')
+    const sass = require('gulp-sass')(require('sass'))
+    const purgecss = require('gulp-purgecss')
 
-  function buildStyles(){
-      return src('felix/**/*.scss')
-      .pipe(sass())
-      .pipe(purgecss({content:['*.html']}))
-      .pipe(dest('./css'))
-  }
+    function buildStyles(){
+        return src('felix/**/*.scss')
+        .pipe(sass())
+        .pipe(purgecss({content:['*.html']}))
+        .pipe(dest('./css'))
+    }
 
-  function watchTask(){
-    
-    watch(['felix/**/*.scss'],buildStyles)
-  }
+    function watchTask(){
+      
+      watch(['felix/**/*.scss'],buildStyles)
+    }
 
-  exports.default = series(buildStyles,watchTask)
-  exports.build = series(buildStyles)
+    exports.default = series(buildStyles,watchTask)
+    exports.build = series(buildStyles)
