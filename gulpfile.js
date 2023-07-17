7f8ee82fa58e5fef55befbe9e9f40b2f78aa278f
Felix Owolabi <owolabifelix78@gmail.com>
2023-07-17 13:34:38 +0100

    cssLibrary

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,12 @@
   const { src,watch,dest,series } = require('gulp')
   const sass = require('gulp-sass')(require('sass'))
   const purgecss = require('gulp-purgecss')
-  // const deploy = require('gulp-gh-pages');
-
-  // gulp.task('deploy', function () {
-  //   return gulp.src("./prod/**/*")
-  //     .pipe(deploy({ 
-  //       remoteUrl: "https://github.com/JustFelix-Dev/JustFelix-Dev.github.io.git",
-  //       branch: "master"
-  //     }))
-  // });
 
   function buildStyles(){
       return src('felix/**/*.scss')
       .pipe(sass())
       .pipe(purgecss({content:['*.html']}))
-      .pipe(dest('css'))
+      .pipe(dest('./css'))
   }
 
   function watchTask(){
